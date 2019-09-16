9c54d92bcf6f56b2cbb17fe541296839f94257f4
André Ghisleni Raimann <andre@andreg.com.br>
2019-09-16 16:18:10 -0300

    Client Ip

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get('/', (req, res)=>{
   res.send("<h1>Hello World.</h1>");
+  console.log(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
 })
                                 
 const port = process.env.PORT || 3000;
